e6957ca82bec6da1c0b5311b9a0667f78678ac73
Laura <lmcsay@gmail.com>
2020-11-27 19:09:07 -0600

    Add error page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from django.http import HttpResponse
 
 from . import util
 
@@ -12,8 +13,12 @@ def index(request):
     })
 
 def title(request, title):
-
-    htmlTitle = markdowner.convert(util.get_entry(title))
+    try:
+        htmlTitle = markdowner.convert(util.get_entry(title))
+    except: 
+        return render(request, "encyclopedia/404.html", {
+            "title": title
+        })
     return render(request, "encyclopedia/title.html", {
         "title": htmlTitle,
         "entry_name": title
